6ca205a0c3e0ee3e1531174927952ca1e03918f2
David Golden <dagolden@cpan.org>
2013-10-28 17:56:36 -0400

    import

---
+++ lib/URI/Tiny.pm
@@ -0,0 +1,33 @@
+use v5.10;
+use strict;
+use warnings;
+
+package URI::Tiny;
+# ABSTRACT: small, simple, correct URI parsing and generation
+# VERSION
+
+1;
+
+=for Pod::Coverage BUILD
+
+=head1 SYNOPSIS
+
+    use URI::Tiny;
+
+=head1 DESCRIPTION
+
+This module might be cool, but you'd never know it from the lack
+of documentation.
+
+=head1 USAGE
+
+Good luck!
+
+=head1 SEE ALSO
+
+=for :list
+* Maybe other modules do related things.
+
+=cut
+
+# vim: ts=4 sts=4 sw=4 et:
